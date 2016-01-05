594bd69bbae9d37c596ae38a6ee6091a2dd447b2
Gary Pastor <gary.pastor@dot.co.za>
2016-01-05 11:05:59 +0200

    Initial commit

---
+++ age.py
@@ -0,0 +1,9 @@
+age=10
+if age <10:
+    print ("You are under 10")
+elif age==10:
+    print ("You are 10")
+else:
+    print ("You are over 10")
+exit()
+
---
+++ vba_extract.py
@@ -0,0 +1,5 @@
+#!c:\python34\python.exe
+# EASY-INSTALL-SCRIPT: 'XlsxWriter==0.7.3','vba_extract.py'
+__requires__ = 'XlsxWriter==0.7.3'
+import pkg_resources
+pkg_resources.run_script('XlsxWriter==0.7.3', 'vba_extract.py')
